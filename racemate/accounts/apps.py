7d65b7995970b24628cd19be6617617323cbf855
Akshay Pratap Singh <mrprabalpratap23@gmail.com>
2025-11-28 17:21:35 +0530

    Fix all apps.py + __init__.py – final Vercel deploy

--- racemate/accounts/apps.py
+++ racemate/accounts/apps.py
@@ -1,6 +1,14 @@
+# from django.apps import AppConfig
+
+
+# class AccountsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'accounts'
+
+# racemate/accounts/apps.py
 from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
+    name = 'racemate.accounts'   # ← THIS LINE WAS WRONG BEFORE
